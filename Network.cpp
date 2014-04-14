91feadb60f1845f722312f3534fe476040f75035
ellaqezi <ellaqezi@gmail.com>
2014-04-14 20:02:38 +0200

    countActive() complete, countConnected() needs work

--- Network.cpp
+++ Network.cpp
@@ -22,6 +22,13 @@ Network::Network(Network const &other) {
 		_proteins->push_back(new Protein((other._proteins->at(i))));
 	}
 }
+Network::Network(vector<Protein*> &other) {
+	_proteins = new vector<Protein*>;
+	_proteins->reserve(other.size());
+	for (int i = 0; i < other.size(); i++) {
+		_proteins->push_back(new Protein((other.at(i))));
+	}
+}
 Network::~Network() {
 	delete _proteins;
 }
@@ -124,11 +131,12 @@ Network& Network::fixedPoint(map<string, string> &edges,
 	this->reset();
 	this->setStates(strState);
 	string prevNetState = "";
-    set<string> setOfStates;
+	set<string> setOfStates;
 	while (setOfStates.count(this->state()) <= 0) {
-        setOfStates.insert(this->state());
-        prevNetState = this->state();
-		for (vector<Protein*>::iterator pit = _proteins->begin(); pit != _proteins->end(); pit++) {
+		setOfStates.insert(this->state());
+		prevNetState = this->state();
+		for (vector<Protein*>::iterator pit = _proteins->begin();
+				pit != _proteins->end(); pit++) {
 			(**pit).tsLevel((**pit).state());
 		}
 		fpCount++;
@@ -145,7 +153,9 @@ Network& Network::fixedPoint(map<string, string> &edges,
 	}
 	for (vector<Protein*>::iterator it = _proteins->begin();
 			it != _proteins->end(); it++) {
-		(**it).setTSLevel(((**it).tsLevel() - prevGeneExpLevels.at((**it).name()))/fpCount + prevGeneExpLevels.at((**it).name()));
+		(**it).setTSLevel(
+				((**it).tsLevel() - prevGeneExpLevels.at((**it).name()))
+						/ fpCount + prevGeneExpLevels.at((**it).name()));
 	}
 
 //	cout << "\t<<Stationary @ " << fpCount << " " << this->state() << "\n";
@@ -230,11 +240,12 @@ int Network::sum(set<string> posRegulators, set<string> negRegulators) {
 	return sumOfStates;
 }
 Protein& Network::find(string protein) {
-	Protein *p;
-	for (vector<Protein*>::iterator it = _proteins->begin();
-			it != _proteins->end(); it++) {
+	Protein *p = NULL;
+	for (vector<Protein*>::iterator it = this->_proteins->begin();
+			it != this->_proteins->end(); it++) {
 		if ((**it).name() == protein) {
 			p = *it;
+			return *p;
 		}
 	}
 	return *p;
@@ -252,82 +263,89 @@ ostream& Network::print(ostream& os) {
 	}
 	return os;
 }
-void Network::graph(const char *fileName) {
+void Network::graph(const char *fileName, Network& network) {
 	ofstream gdot;
 	gdot.open(fileName);
-	gdot << *this;
+	gdot << network;
 	gdot.close();
+}
+void Network::graph(const char *fileName) {
+	graph(fileName, *this);
 }
 void Network::graph() {
 	this->graph("graph.gv");
 }
-void Network::graphBasins(const char *fileName, map<string, map<string,string>*> basins, map<string, int> edgeCount) {
-    ofstream ofs;
+void Network::graphBasins(const char *fileName,
+		map<string, map<string, string>*> basins, map<string, int> edgeCount) {
+	ofstream ofs;
 	ofs.open(fileName);
 	ofs << "digraph " << "G" << " {" << endl
-    << "layers=\"basins:edges:singles\";\n";
-    
-    //attractors with labels
-    for (map<string, map<string, string>*>::iterator it = basins.begin();
-         it != basins.end(); it++) {
+			<< "layers=\"basins:edges:singles\";\n";
+
+	//attractors with labels
+	for (map<string, map<string, string>*>::iterator it = basins.begin();
+			it != basins.end(); it++) {
 //		ofs << "\t\t\t\"" << it->first << "\"\t" << it->second->size() + 1 << endl;
 		ofs
-        << "subgraph {\nnode[shape=box, style=unfilled, layer=\"basins\"];\n";
+				<< "subgraph {\nnode[shape=box, style=unfilled, layer=\"basins\"];\n";
 		ofs << "\"" << it->first << "\" ;\n";
 		//edges that occur > 3 times
 		ofs
-        << "node[shape=point, layer=\"edges\", color=dodgerblue2, width=0.15, style=filled];\nedge[color=blue];\n";
+				<< "node[shape=point, layer=\"edges\", color=dodgerblue2, width=0.15, style=filled];\nedge[color=blue];\n";
 		ostringstream o;
 		for (map<string, string>::iterator itm = it->second->begin();
-             itm != it->second->end(); itm++) {
+				itm != it->second->end(); itm++) {
 			if (edgeCount.at(itm->first) > 3) {
 				ofs << "\"" << itm->second << "\" [width="
-                << log(edgeCount.at(itm->second)) / 15 << "];\n";
+						<< log(edgeCount.at(itm->second)) / 15 << "];\n";
 				ofs << "\"" << itm->first << "\" -> \"" << itm->second << "\" "
-                << "[penwidth=" << log(edgeCount.at(itm->first)) * 2
-                <</* ", len="<< log(edgeCount.at(itm->first)) <<*/"];\n";
+						<< "[penwidth=" << log(edgeCount.at(itm->first)) * 2
+						<</* ", len="<< log(edgeCount.at(itm->first)) <<*/"];\n";
 			} else {
 				o << "\"" << itm->first << "\" -> \"" << itm->second << "\";\n";
 			}
 		}
 		//edges that occur <= 3 times
 		ofs
-        << "node[layer=\"singles\", color=gray25, width=0.05];\nedge[color=gray50, len=1];\n";
+				<< "node[layer=\"singles\", color=gray25, width=0.05];\nedge[color=gray50, len=1];\n";
 		ofs << o.str() << "}\n";
 	}
 	ofs << "}";
 	ofs.close();
 
 }
-void Network::findAttractors(map<string, map<string, string> *>& basins, map<string, int>& edgeCount) {
-    int networkSize = this->numProteins();
-    for (int i = 0; i < pow(2, networkSize); i++) { //determine fixed point basin size
+void Network::findAttractors(map<string, map<string, string> *>& basins,
+		map<string, int>& edgeCount) {
+	int networkSize = this->numProteins();
+	for (int i = 0; i < pow(2, networkSize); i++) { //determine fixed point basin size
 		map<string, string>* edges;
 		edges = new map<string, string>;
 		this->fixedPoint(*edges, edgeCount, this->binStr(i));
 		if (basins.count(this->state()) <= 0) {
 			basins.insert(
-                          pair<string, map<string, string>*>(this->state(), edges));
+					pair<string, map<string, string>*>(this->state(), edges));
 		} else {
 			basins.at(this->state())->insert(edges->begin(), edges->end());
 		}
 	}
 }
-void Network::printAttractors(map<string, map<string, string> *> basins, std::ostream &os) {
-    for (map<string, map<string, string>*>::iterator it = basins.begin();
-         it != basins.end(); it++) {
-		os << "\t\t\t\"" << it->first << "\"\t" << it->second->size() + 1 << endl;
+void Network::printAttractors(map<string, map<string, string> *> basins,
+		std::ostream &os) {
+	for (map<string, map<string, string>*>::iterator it = basins.begin();
+			it != basins.end(); it++) {
+		os << "\t\t\t\"" << it->first << "\"\t" << it->second->size() + 1
+				<< endl;
 	}
 }
 void Network::setStateTS() {
-    float meanExpLevel = 0;
+	float meanExpLevel = 0;
 	for (vector<Protein*>::iterator it = this->_proteins->begin();
-         it != this->_proteins->end(); it++) {
+			it != this->_proteins->end(); it++) {
 		meanExpLevel += (**it).tsLevel();
 	}
 	meanExpLevel /= this->_proteins->size();
 	for (vector<Protein*>::iterator it = this->_proteins->begin();
-         it != this->_proteins->end(); it++) {
+			it != this->_proteins->end(); it++) {
 		if ((**it).tsLevel() > meanExpLevel) {
 			(**it).state(1);
 		} else {
@@ -336,23 +354,23 @@ void Network::setStateTS() {
 	}
 }
 void Network::setStateAB(map<string, map<string, string> *> basins) {
-    for (map<string, map<string, string>*>::iterator bit = basins.begin();
-         bit != basins.end(); bit++) {
-        this->reset();
-        this->setStates((bit)->first);
-        for (vector<Protein*>::iterator it = this->_proteins->begin();
-             it != this->_proteins->end(); it++) {
-            (*it)->abLevel((*it)->state() * ((bit)->second->size()+1));
-        }
-    }
-    float meanExpLevel = 0;
+	for (map<string, map<string, string>*>::iterator bit = basins.begin();
+			bit != basins.end(); bit++) {
+		this->reset();
+		this->setStates((bit)->first);
+		for (vector<Protein*>::iterator it = this->_proteins->begin();
+				it != this->_proteins->end(); it++) {
+			(*it)->abLevel((*it)->state() * ((bit)->second->size() + 1));
+		}
+	}
+	float meanExpLevel = 0;
 	for (vector<Protein*>::iterator it = this->_proteins->begin();
-         it != this->_proteins->end(); it++) {
+			it != this->_proteins->end(); it++) {
 		meanExpLevel += (**it).abLevel();
 	}
 	meanExpLevel /= this->_proteins->size();
 	for (vector<Protein*>::iterator it = this->_proteins->begin();
-         it != this->_proteins->end(); it++) {
+			it != this->_proteins->end(); it++) {
 		if ((**it).abLevel() > meanExpLevel) {
 			(**it).state(1);
 		} else {
@@ -364,24 +382,40 @@ ostream& Network::basins(const char *fileName, ostream& os) {
 	map<string, map<string, string> *> basins;
 	map<string, int> edgeCount;
 
-    //determine fixed point basin size
+	//determine fixed point basin size
 	findAttractors(basins, edgeCount);
-    
-//    printAttractors(basins, os);
-    
-    //graph basins
-    graphBasins(fileName, basins, edgeCount);
-    
-    //set states according to attractor*basin size
-    setStateAB(basins);
-    
-    //set states according to time series expression levels
-	setStateTS();
 
-    //graph according to time series expression levels
+//    printAttractors(basins, os);
+
+	//graph basins
+	graphBasins(fileName, basins, edgeCount);
+
+	//set states according to attractor*basin size
+	setStateAB(basins);
+	Network *ab = countActive();
+	cout << "connected/active: " << countConnected(*ab) << " / "
+			<< ab->numProteins() << endl;
+//	ab->print();
+
+	//graph according to attractor*basin size expression levels
+	string abGraph("attractorBasin");
+	abGraph.append(this->state());
+	this->graph(this->createGV(abGraph));
+
+	//set states according to time series expression levels
+	setStateTS();
+	Network *ts = countActive();
+	cout << "connected/active: " << countConnected(*ts) << " / "
+			<< ts->numProteins() << endl;
+	ts->print();
+
+	//graph according to time series expression levels
 	string tsGraph("timeSeries");
-	tsGraph.append(fileName);
+	tsGraph.append(this->state());
 	this->graph(this->createGV(tsGraph));
+
+	delete ab;
+	delete ts;
 	return os;
 }
 ostream& Network::basins(ostream& os) {
@@ -389,17 +423,17 @@ ostream& Network::basins(ostream& os) {
 	map<string, map<string, string> *> basins;
 	map<string, int> edgeCount;
 
-    //determine fixed point basin size
+	//determine fixed point basin size
 	findAttractors(basins, edgeCount);
-    
-    printAttractors(basins, os);
-    
-    //set states according to attractor*basin size
-    setStateAB(basins);
-	
-    //set states according to time series expression levels
+
+	printAttractors(basins, os);
+
+	//set states according to attractor*basin size
+	setStateAB(basins);
+
+	//set states according to time series expression levels
 	setStateTS();
-    
+
 	return os;
 }
 void Network::basins() {
@@ -423,6 +457,49 @@ string Network::binStr(unsigned n, int length) {
 		lead += '0';
 	}
 	return lead + res;
+}
+
+Network* Network::countActive() {
+	vector<Protein*> active;
+	for (vector<Protein*>::iterator it = this->_proteins->begin();
+			it != this->_proteins->end(); it++) {
+		if ((**it).state() == 1) {
+			active.push_back(*it);
+		}
+	}
+	Network *n = new Network(active);
+	return n;
+}
+int Network::countConnected(Network& network) {
+	int count = 0;
+	for (vector<Protein*>::iterator it = network._proteins->begin();
+			it != network._proteins->end(); it++) {
+		bool connected = false;
+		set<string>::iterator si = (**it).activatedBy().begin();
+		while (!connected and si != (**it).activatedBy().end()) {
+			string protein = *si;
+			cout << protein << endl;
+			if (&(network.find(protein)) != NULL) {
+				si = (**it).activatedBy().end();
+				connected = true;
+			}
+			si++;
+		}
+//		if (!connected) {
+//			set<string>::iterator si = (**it).deactivatedBy().begin();
+//			while (!connected and si != (**it).deactivatedBy().end()) {
+//				if (&find(*si) != NULL) {
+//					si = (**it).deactivatedBy().end();
+//					connected = true;
+//				}
+//				si++;
+//			}
+
+		if (connected) {
+			count += 1;
+		}
+	}
+	return count;
 }
 string Network::binStr(unsigned n) {
 	return binStr(n, this->numProteins());
@@ -448,7 +525,7 @@ ostream& operator<<(ostream& os, const Network& network) {
 	return os;
 }
 ofstream& operator<<(ofstream& ofs, const Network& network) {
-	ofs << "digraph " << network.state() << "  {\n";
+	ofs << "digraph \"" << network.state() << "\"  {\n";
 	for (vector<Protein*>::iterator it = network._proteins->begin();
 			it != network._proteins->end(); it++) {
 		ofs << (**it);
--- Network.h
+++ Network.h
@@ -23,6 +23,7 @@ public:
 	Network();
 	Network(int size);
 	Network(Network const &other);
+	Network(std::vector<Protein*> &proteins);
 	virtual ~Network();
 
 	Network& addProtein(Protein protein);
@@ -44,18 +45,27 @@ public:
 	int sum(std::set<std::string> regulators);
 	int sum(std::set<std::string> posRegulators, std::set<std::string> negRegulators);
     Protein& find(std::string protein);
-	void print();
+
+    void print();
 	std::ostream& print(std::ostream& os);
+
 	void graph();
 	void graph(const char *fileName);
-    void graphBasins(const char *fileName,std::map<std::string, std::map<std::string, std::string> *> basins, std::map<std::string, int> edgeCount);
+	void graph(const char *fileName, Network& network);
+	void graphBasins(const char *fileName,std::map<std::string, std::map<std::string, std::string> *> basins, std::map<std::string, int> edgeCount);
+
     void findAttractors(std::map<std::string, std::map<std::string, std::string> *>& basins, std::map<std::string, int> &edgeCount);
     void printAttractors(std::map<std::string, std::map<std::string, std::string> *> basins, std::ostream& os);
-	void setStateTS();
+
+    void setStateTS();
     void setStateAB(std::map<std::string, std::map<std::string, std::string> *> basins);
+
     void basins();
 	std::ostream& basins(std::ostream& os);
     std::ostream& basins(const char *fileName, std::ostream& os);
+
+    Network* countActive();
+    int countConnected(Network& network);
 
     std::string binStr(unsigned n, int length);
 	std::string binStr(unsigned n);
